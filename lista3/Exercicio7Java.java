d280bafae8a4fd89be2a6954be8ea72edabb4980
Victor040810 <38638444+Victor040810@users.noreply.github.com>
2018-06-16 22:23:39 -0300

    Add files via upload

---
+++ lista3/Exercicio7Java.java
@@ -0,0 +1,43 @@
+
+package exercicio7java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vycto
+ */
+public class Exercicio7Java {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x,y;
+        float m1 = 0, m2 = 0, soma = 0;
+        System.out.println(" Informe a quantidade de Alunos da turma: ");
+        x = input.nextInt();
+        for (y = 0; y <= x; y++) {
+            System.out.println(" Informe  a media de cada aluno");
+            int z = input.nextInt();
+
+            if (m1 == 0) {
+                m1 = z;
+                m2 = z;
+            }
+            if (z > m1) {
+                m1 = z;
+            } else if (z < m2) {
+                m2 = z;
+            }
+            if (z < 0) {
+                y = x;
+            }
+            soma = soma + z;
+        }
+        System.out.println("A maior media: " + m1);
+        System.out.println("A menor media: " + m2);
+        System.out.println("A media aritimetica da turma: " + (soma) / x);
+    }
+  }
+    
+
